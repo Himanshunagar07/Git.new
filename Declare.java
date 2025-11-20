ed4f095dbb19940bf9f9772c94d2364d8652b9ab
Himanshunagar07 <himanshunagar5507@gmail.com>
2025-11-20 22:34:49 +0530

    Create Declare.java
    Declaration of all Data Type in Java

---
+++ Declare.java
@@ -0,0 +1,31 @@
+public class Main{
+  public static void main(String[]args){
+    byte b=100;
+    short s=30000;
+    int i=123456;
+    long l=9876543210l;
+    float f=5.75f;
+    double d=99.99;
+    char c='A';
+    boolean bool=true;
+
+  System.out.println("byte value:" + b);
+  System.out.println("short value:" + s);
+  System.out.println("int value:" + i);
+  System.out.println("long value:" + l);
+  System.out.println("float value:" + f);
+  System.out.println("double value:" + d);
+  System.out.println("char value:" + c);
+  System.out.println("boolean value:" + bool);
+  }
+}
+
+Output:
+byte value:100
+short value:30000
+int value:123456
+long value:9876543210
+float value:5.75
+double value:99.99
+char value:A
+boolean value:true
