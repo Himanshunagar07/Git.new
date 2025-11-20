33b89356b8a372b448fe58578f09c1d3f22ae343
Himanshunagar07 <himanshunagar5507@gmail.com>
2025-11-20 23:16:56 +0530

    Create Type_casting.java
    Casting of double data type to integer data type.

---
+++ Type_casting.java
@@ -0,0 +1,14 @@
+public class main{
+    public static void main(String[]args){
+        
+        double num= 9.78;
+        int CastedNum=(int)num;
+        
+        System.out.println("Orignal double value:" + num);
+        System.out.println("After casting to int:" + CastedNum);
+    }
+}
+
+Output:
+Orignal double value:9.78
+After casting to int:9
