b8e89a7fd42bfebe01df3a2ab21fce94b9b32885
Himanshunagar07 <himanshunagar5507@gmail.com>
2025-11-20 22:26:34 +0530

    Create Displaycollage.java

---
+++ Displaycollage.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        String college = "SAIT COLLEGE";
+        for (int i = 0; i < college.length(); i++) {
+            System.out.println(college.charAt(i));
+        }
+    }
+}
+
+Output:
+S
+A
+I
+T
+ 
+C
+O
+L
+L
+E
+G
+E
